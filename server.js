ce583e3a67e3c8ffd0997574cbbdd21ffa21ca3f
enochwongdh <enochwong.dh@gmail.com>
2026-01-22 17:04:16 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send("Hello world~!");
+})
+
+app.get('/test', (req, res) => {
+    res.send("test page");
+})
+
+app.listen(PORT, HOST, () => {
+    console.log(`Running on http:${HOST}:${PORT}`);
+})
